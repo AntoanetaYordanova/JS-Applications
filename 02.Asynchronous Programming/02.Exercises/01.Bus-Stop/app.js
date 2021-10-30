75a3c83178996255f1325cb879db543d48cdf718
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-10-30 18:57:28 +0200

    JS Exerses

--- 02.Asynchronous Programming/02.Exercises/01.Bus-Stop/app.js
+++ 02.Asynchronous Programming/02.Exercises/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
 function getInfo() {
-    console.log("TODO...");
+    manipualteData();
+
+    async function manipualteData() {
+        const list = document.getElementById('buses');
+        list.innerHTML = '';
+        const busId = document.getElementById('stopId').value;
+        try {
+            const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${busId}`);
+            if(!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            for(let bus in data.buses) {
+                const li = document.createElement('li');
+                li.textContent = `Bus ${bus} arrives in ${data.buses[bus]} minutes`;
+                list.appendChild(li);
+            }
+
+        } catch(e) {
+            list.textContent = e.message;
+        }
+    }
 }
