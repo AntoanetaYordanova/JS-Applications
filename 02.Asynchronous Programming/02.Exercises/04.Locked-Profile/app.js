8e34c73e72f53476820977dbbe52694b5c9e0d04
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2021-11-01 10:46:15 +0100

    JS Exerses

--- 02.Asynchronous Programming/02.Exercises/04.Locked-Profile/app.js
+++ 02.Asynchronous Programming/02.Exercises/04.Locked-Profile/app.js
@@ -1,3 +1,32 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    const usersResponse = await fetch('http://localhost:3030/jsonstore/advanced/profiles');
+    const usersData = await usersResponse.json();
+    const usersKeys = Object.keys(usersData);
+    const mainEl =  document.getElementById('main');
+    mainEl.innerHTML = '';
+
+    usersKeys.forEach(u => {
+        const currentUser = usersData[u];
+        const divEl = document.createElement('div');
+        divEl.classList.add('profile');
+        divEl.innerHTML = `<img src="./iconProfile2.png" class="userIcon" /><label>Lock</label><input type="radio" name="user1Locked" value="lock" checked><label>Unlock</label><input type="radio" name="user1Locked" value="unlock"><br><hr><label>Username</label><input type="text" name="user1Username" value="${currentUser.username}" disabled readonly /><div id="user1HiddenFields"><hr><label>Email:</label><input type="email" name="user1Email" value="${currentUser.email}" disabled readonly /><label>Age:</label><input type="email" name="user1Age" value="${currentUser.age}" disabled readonly />`;
+        const buttonEl = document.createElement('button');
+        buttonEl.textContent = 'Show more';
+        buttonEl.addEventListener('click', showHideInfo);
+        divEl.appendChild(buttonEl);
+        mainEl.appendChild(divEl);
+    });
+
+    function showHideInfo(ev) {
+        const radioInput = ev.target.parentElement.querySelector('input:nth-child(5)');
+        if(radioInput.checked) {
+            if(ev.target.textContent === 'Show more') {
+                ev.target.textContent = 'Hide it';
+                ev.target.previousSibling.style.display = 'inline';
+            } else {
+                ev.target.textContent = 'Show more';
+                ev.target.previousSibling.style.display = 'none';
+            }
+        }
+    }
 }
--- 02.Asynchronous Programming/02.Exercises/05. Accordion/accordion.js
+++ 02.Asynchronous Programming/02.Exercises/05. Accordion/accordion.js
@@ -1,3 +1,50 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+    const headDataResponse = await fetch(url);
+    const headData = await headDataResponse.json();
+    const mainEl = document.getElementById('main');
+
+    headData.forEach(d => {
+      const button = createEl('button', 'button', 'more');
+      button.addEventListener('click', moreLessInfo);
+      button.id = d._id;
+      const div = createEl('div', 'accordion', createEl('div', 'head', createEl('span', undefined, d.title), button), createEl('div', 'extra', createEl('p')));
+      mainEl.appendChild(div);
+    });
+
+    async function moreLessInfo(ev) {
+        const hidden = ev.target.parentElement.parentElement.querySelector('.extra');
+        
+        if(ev.target.textContent === 'more') {
+            hidden.style.display = 'inline';
+            ev.target.textContent = 'less';
+            const id = ev.target.id;
+            const moreInfoResponse = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${id}`);
+            const moreInfo = await moreInfoResponse.json();
+            hidden.querySelector('p').textContent = moreInfo.content;
+        } else {
+            hidden.style.display = 'none';
+            ev.target.textContent = 'more';
+        }
+    }
 }
+
+function createEl(type, className, ...content) {
+    const el = document.createElement(type);
+
+    if(className !== undefined) {
+        el.classList.add(className);
+    }
+
+    content.forEach(e => {
+        if(typeof e === 'string') {
+            el.appendChild(document.createTextNode(e));
+        } else {
+            el.appendChild(e);
+        }
+    });
+
+    return el;
+}
+
+solution()
